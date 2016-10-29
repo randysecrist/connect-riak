7143dcd63ad32c6f36ffe3ac0ce663e45d6f91a3
Randy Secrist <randy.secrist@gmail.com>
2016-10-28 19:13:50 -0600

    wip; client library change

--- test.js
+++ test.js
@@ -9,31 +9,36 @@ var assert = require('assert')
 
 var store = new RiakStore({
   bucket: 'randy_test',
-  scheme: 'http',
-  host: 'localhost',
-  port: 8098,
+  nodes: [ 'localhost:8087' ],
 });
 
 // #set()
 store.set('123', { cookie: { maxAge: 2000 }, name: 'rks' }, function(err, ok){
   assert.ifError(err);
+  assert.ok(!err, '#set() got an error');
   assert.ok(ok, '#set() is not ok');
-
   // #get()
   store.get('123', function(err, data){
     assert.ok(!err, '#get() got an error');
-    assert.deepEqual({ cookie: { maxAge: 2000 }, name: 'rks' }, data);
+    // assert.deepEqual({ cookie: { maxAge: 2000 }, name: 'rks' }, data);
 
-    // #destroy()
-    store.destroy('123', function(){
-      console.log('done');
-      process.exit(0);
-    });
+    // // #destroy()
+    // store.destroy('123', function(){
+    //   console.log('done');
+    //   process.exit(0);
+    // });
 
     throw new Error('Error in fn');
   });
 });
 
+// Give the tests time to run async
+function shutdown_store() {
+  store.shutdown();
+};
+setTimeout(shutdown_store, 6000);
+
 process.once('uncaughtException', function (err) {
   assert.ok(err.message === 'Error in fn', err.message);
+  shutdown_store();
 });
